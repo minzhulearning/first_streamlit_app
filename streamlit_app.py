39263b06f1e83d719921c38e6b39153164194934
minzhulearning <121968700+minzhulearning@users.noreply.github.com>
2023-01-04 11:53:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
+streamlit.hearder('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Egg')
+
 
